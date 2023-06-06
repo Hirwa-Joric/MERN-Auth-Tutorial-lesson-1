815797b85ae426904328778b1a46a1789e10d0e7
Hirwa-Joric <hirwajoric@gmail.com>
2023-06-06 20:12:20 +0200

    updated

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,14 @@
+//Login users
+
+const loginUser = (req, res) => {
+    res.json({mssg:"loging in users"})
+}
+
+// signing in users
+
+const signupUser = (req, res) => {
+    res.json({mssg:"loging in users"})
+}
+
+
+module.exports={signupUser,loginUser}
