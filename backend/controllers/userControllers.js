3fa627c7cc4f461b05de1b3033f764d1fad7f227
Hirwa-Joric <hirwajoric@gmail.com>
2023-06-07 20:12:20 +0200

    updated

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,7 +1,7 @@
 const User = require("../models/userModel")
 const jwt = require("jsonwebtoken")
 
-const createToken = (_id){
+const createToken = (_id)=>{
   return jwt.sign({_id},process.env.SECRETE , {expiresIn: "3d"})
 }
 //Login users
