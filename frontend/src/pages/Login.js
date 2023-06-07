18b9e9fa6c4b4181ae9f8a21b6dee40b95c3803a
Hirwa-Joric <hirwajoric@gmail.com>
2023-06-07 20:12:20 +0200

    updated

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-const Signup = () => {
+const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
@@ -11,8 +11,8 @@ const Signup = () => {
   }
 
   return (
-    <form className="signup" onSubmit={handleSubmit}>
-      <h3>Sign Up</h3>
+    <form className="login" onSubmit={handleSubmit}>
+      <h3>Log In</h3>
       
       <label>Email address:</label>
       <input 
@@ -27,9 +27,9 @@ const Signup = () => {
         value={password} 
       />
 
-      <button>Sign up</button>
+      <button>Log in</button>
     </form>
   )
 }
 
-export default Signup
+export default Login
