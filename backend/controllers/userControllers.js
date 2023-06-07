bbc61d06b4a69a06a3b6fe53e6e01804350d5c89
Hirwa-Joric <hirwajoric@gmail.com>
2023-06-07 20:12:20 +0200

    updated

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,9 @@
 const User = require("../models/userModel")
 const jwt = require("jsonwebtoken")
+
+const createToken = (_id){
+  return jwt.sign({_id},process.env.SECRETE , {expiresIn: "3d"})
+}
 //Login users
 
 const loginUser = (req, res) => {
@@ -19,7 +23,10 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password)
 
-    res.status(200).json({email, user})
+    // create User
+    const token = createToken(user._id)
+  
+    res.status(200).json({email, token})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
