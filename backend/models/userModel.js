6f6d670c68dec0e81bdfdad52972fc8f7f36a2b7
Hirwa-Joric <hirwajoric@gmail.com>
2023-06-07 20:12:20 +0200

    updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -13,5 +13,48 @@ const userSchema = new Schema({
                 required: true
     }
 })
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+// static signup method
+userSchema.statics.signup = async function(email, password) {
+
+  const exists = await this.findOne({ email })
+
+  if (exists) {
+    throw Error('Email already in use')
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+
+  const user = await this.create({ email, password: hash })
+
+  return user
+}
+
+module.exports = mongoose.model('User', userSchema)
+// user static method for signup user
+
+userSchema.static.signup = async (email, password){
+    const exists = await this.findOne({ email })
+    if (exitst) {
+        throw  Error("User already exists")
+    }
+}
 
 module.exports = mongoose.model("User", userSchema)
